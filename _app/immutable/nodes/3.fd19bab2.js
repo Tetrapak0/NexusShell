c46cf9f300ecac697e6e17d37355f0a0e8e1ab71
Tetrapak0 <112382253+Tetrapak0@users.noreply.github.com>
2023-11-28 18:05:30 +0100

    Updates

---
+++ _app/immutable/nodes/3.fd19bab2.js
@@ -0,0 +1,8 @@
+import{s as o,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as n,i as h,g as i,h as p,x as d,k as u,a as c,f as m}from"../chunks/index.4d21fe7b.js";function x(l){let e,s=`<h1>Downloads</h1> <p>Visit <a href="https://github.com/Tetrapak0/NexusShell/releases/latest/">GitHub</a> to download the latest release.</p> <p>Or download from here:<br/>
+            Server:<br/>
+                <a href="https://github.com/Tetrapak0/NexusShell/releases/latest/download/NexusShell.exe">NexusShell.exe</a> <br/>
+                <a href="https://github.com/Tetrapak0/NexusShell/releases/latest/download/icon.ico">icon.ico</a>
+            (Required for system tray icon to appear)
+        <br/> <br/>
+            Client:<br/>
+                <a href="https://github.com/Tetrapak0/NexusShell/releases/latest/download/NexusShell-Client-x86_64">NexusShell-Client-x86_64</a> <br/></p>`;return{c(){e=i("div"),e.innerHTML=s,this.h()},l(t){e=p(t,"DIV",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-sh2tq7"&&(e.innerHTML=s),this.h()},h(){u(e,"class","downloads-page")},m(t,r){c(t,e,r)},p:a,i:a,o:a,d(t){t&&m(e)}}}class S extends n{constructor(e){super(),h(this,e,null,x,o,{})}}export{S as component};
