e1e5c67df620d4c46f54bf29e59db942ecbe3a3d
Tetrapak0 <112382253+Tetrapak0@users.noreply.github.com>
2023-12-02 15:40:19 +0100

    Alpha 1.4 - Server
    - Custom port support
    - Switched to atomic<bool>
    - Fixed rare traypp init bug (traypp will be replaced in a future release)
    - Fixed some config file bugs

--- Server/src/Config.cpp
+++ Server/src/Config.cpp
@@ -3,17 +3,17 @@
 #include "../include/GUI.h"
 
 bool button_cleared = false;
-bool ids_locked = false;
+atomic<bool> ids_locked;
 
 unordered_map<string, id> ids = {};
 
-void read_config(json& config, id& ID) {
-	if (config[config.begin().key()].contains("nickname")) ID.nickname = config[config.begin().key()]["nickname"].get<string>();
-	if (config[config.begin().key()].contains("profiles")) {
+void read_config(id& ID) {
+	if (ID.config[ID.config.begin().key()].contains("nickname")) ID.nickname = ID.config[ID.config.begin().key()]["nickname"].get<string>();
+	if (ID.config[ID.config.begin().key()].contains("profiles")) {
 		ID.profiles.clear();
 		int profile_count = 0;
-		for (auto& profile : config[config.begin().key()]["profiles"]) if (profile.is_object()) profile_count++;
-		if (profile_count == 0) {
+		for (auto& profile : ID.config[ID.config.begin().key()]["profiles"]) if (profile.is_object()) profile_count++;
+		if (!profile_count) {
 			profile profile1;
 			for (int a = 0; a < profile1.columns * profile1.rows; a++)
 				profile1.buttons.push_back(button());
@@ -22,24 +22,24 @@ void read_config(json& config, id& ID) {
 		}
 		for (int i = 0; i < profile_count; ++i) {
 			profile profile1;
-			json profile_store = config[config.begin().key()]["profiles"][to_string(i)];
+			json& profile_store = ID.config[ID.config.begin().key()]["profiles"][to_string(i)];
 			if (profile_store.contains("columns")) profile1.columns = std::stoi(profile_store["columns"].get<string>());
 			if (profile_store.contains("rows"))    profile1.rows = std::stoi(profile_store["rows"].get<string>());
 			if (profile_store.contains("pages")) {
 				int page_count = 0;
 				for (auto& page : profile_store["pages"]) if (page.is_object()) page_count++;
-				if (page_count == 0) {
+				if (!page_count) {
 					for (int a = 0; i < profile1.columns * profile1.rows; ++i) 
 						profile1.buttons.push_back(button());
 					continue;
 				}
 				for (int j = 0; j < page_count; ++j) {
-					json page_store = profile_store["pages"][to_string(j)];
+					json& page_store = profile_store["pages"][to_string(j)];
 					if (page_store.contains("buttons")) {
 						for (int k = 0; k < profile1.columns * profile1.rows; ++k) {
 							button button1;
 							if (page_store["buttons"].contains(to_string(k))) {
-								json button_store = page_store["buttons"][to_string(k)];
+								json& button_store = page_store["buttons"][to_string(k)];
 								if (button_store.contains("label")) button1.label = button_store["label"];
 								button1.label_backup = button1.label;
 								if (button_store.contains("has default label")) {
@@ -53,15 +53,14 @@ void read_config(json& config, id& ID) {
 									else if (button_store["type"] == "1") button1.type = button::types::URL;
 									else if (button_store["type"] == "2") button1.type = button::types::Command;
 									else if (button_store["type"] == "3") button1.type = button::types::Directory;
-									else button1.type = button::types::File;
-								} else button1.type = button::types::File;
+								}
 								if (button_store.contains("action")) button1.action = button_store["action"];
 							}
 							profile1.buttons.push_back(button1);
 						}
-					}
+					} else for (int i = 1; i <= profile1.columns * profile1.rows; ++i) profile1.buttons.push_back(button());
 				}
-			}
+			} else for (int i = 1; i <= profile1.columns * profile1.rows; ++i) profile1.buttons.push_back(button());
 			ID.profiles.push_back(profile1);
 		}
 	} else {
@@ -73,24 +72,26 @@ void read_config(json& config, id& ID) {
 }
 
 int configure_id(id& ID) {
-	ID.config_file = getenv("USERPROFILE");
+	ID.config_file = getenv("USERPROFILE");								// TODO: Switch to the client way of file & dir creation
 	ID.config_file += "\\AppData\\Roaming\\NexusShell\\";
 	if (!exists(ID.config_file)) create_directory(ID.config_file);
 	ID.config_file += ID.ID;
 	ID.config_file += ".json";
-	cerr << ID.config_file << "\n";
+	LOG(ID.config_file);
 	if (exists(ID.config_file)) {
 		ifstream reader(ID.config_file);
 		if (reader.peek() != ifstream::traits_type::eof()) {
 			try {
 				json config = json::parse(reader);
 				reader.close();
-				read_config(config, ID);
+				ID.config = config;
+				read_config(ID);
 				reconfigure(ID);
-				cerr << "Configured " << ID.nickname << "\n";
+				if (!ID.nickname.empty())	LOGVAR("Configured", ID.nickname);
+				else						LOGVAR("Configured", ID.ID);
 				return 0;
 			} catch (...) {
-				cerr << "invalid config!\n";
+				LOG("invalid config!");
 				send(ID.sock.ClientSocket, "killreason: invalid config", strlen("killreason: invalid config") + 1, 0);
 				closesocket(ID.sock.ClientSocket);
 				return 1;
@@ -102,54 +103,41 @@ int configure_id(id& ID) {
 	for (int i = 1; i <= profile1.columns * profile1.rows; ++i)
 		profile1.buttons.push_back(button());
 	ID.profiles.push_back(profile1);
-	string to_write = "{\"" + ID.ID + "\": {}}";
-	json empty_config = json::parse(to_write);
+	string to_write = "{\"" + ID.ID + "\": {\"profiles\": {\"0\": {\"pages\": {\"0\": {\"buttons\": {}}}}}}}";
 	ofstream writer(ID.config_file);
-	writer << empty_config.dump(4);
+	writer << to_write;
 	writer.close();
+	ID.config = json::parse(to_write);
 	reconfigure(ID);
+	return 0;
 }
 
-int reconfigure(id& ID) {
-	if (exists(ID.config_file)) {
-		ifstream reader(ID.config_file);
-		json config;
-		try {
-			config = json::parse(reader);
-		} catch (...) {
-			cerr << "Invalid Config\n";
-			send(ID.sock.ClientSocket, "killreason: invalid config", strlen("killreason: invalid config") + 1, 0);
-			closesocket(ID.sock.ClientSocket);
-			reader.close();
-			return 1;	// TODO: Change all functions that oculd fail to int and return 1
-		}
-		reader.close();
-		ID.config = config;	// TODO: use instead;
-		if (config[config.begin().key()].contains("nickname"))	config[config.begin().key()].erase("nickname");
-		for (int i = 0; i < ID.profiles.size(); ++i) {
-			json& profile_store = config[config.begin().key()]["profiles"][to_string(i)];
-			if (profile_store.contains("pages")) {
-				int page_count = 0;
-				for (auto& page : profile_store["pages"]) if (page.is_object()) page_count++;
-				if (page_count == 0) continue;
-				for (int j = 0; j < page_count; ++j) {
-					json& page_store = profile_store["pages"][to_string(j)];
-					if (page_store.contains("buttons")) {
-						for (int k = 0; k < ID.profiles[i].columns * ID.profiles[i].rows; ++k) {
-							if (page_store["buttons"].contains(to_string(k))) {
-								json& button_store = page_store["buttons"][to_string(k)];
-								if (button_store.contains("type"))  button_store.erase("type");
-								if (button_store.contains("action")) {
-									if (button_store["action"] == "") {} 
-									else button_store["action"] = "1";
-								}
+void reconfigure(id& ID) {
+	json config = ID.config;
+	if (config[config.begin().key()].contains("nickname"))	config[config.begin().key()].erase("nickname");
+	for (int i = 0; i < ID.profiles.size(); ++i) {
+		json& profile_store = config[config.begin().key()]["profiles"][to_string(i)];
+		if (profile_store.contains("pages")) {
+			int page_count = 0;
+			for (auto& page : profile_store["pages"]) if (page.is_object()) page_count++;
+			if (page_count == 0) continue;
+			for (int j = 0; j < page_count; ++j) {
+				json& page_store = profile_store["pages"][to_string(j)];
+				if (page_store.contains("buttons")) {
+					for (int k = 0; k < ID.profiles[i].columns * ID.profiles[i].rows; ++k) {
+						if (page_store["buttons"].contains(to_string(k))) {
+							json& button_store = page_store["buttons"][to_string(k)];
+							if (button_store.contains("type"))  button_store.erase("type");
+							if (button_store.contains("action")) {
+								if (button_store["action"] == "") {} 
+								else button_store["action"] = "1";
 							}
 						}
 					}
 				}
 			}
 		}
-		string configuration = "cfg" + config.dump();
+		string configuration = "cfg" + ID.config.dump();
 		ID.sock.iSendResult = send(ID.sock.ClientSocket, configuration.c_str(), configuration.length(), 0);
 	}
 }
@@ -164,43 +152,25 @@ void clear_button(int profile, int page, int button) {
 	nxsh_config += "\\";
 	nxsh_config += selected_id;
 	nxsh_config += ".json";
-	json to_remove;
-	ifstream reader(nxsh_config);
-	if (reader && reader.peek() != ifstream::traits_type::eof()) to_remove = json::parse(reader);	// FIXME: no if (reader
-	reader.close();
-	if (to_remove[selected_id]["profiles"][to_string(profile)]["pages"][to_string(page)]["buttons"].contains(to_string(button))) {
-		to_remove[selected_id]["profiles"][to_string(profile)]["pages"][to_string(page)]["buttons"].erase(to_string(button));
+	if (CURRENT_ID.config[selected_id]["profiles"][to_string(profile)]["pages"][to_string(page)]["buttons"].contains(to_string(button))) {
+		CURRENT_ID.config[selected_id]["profiles"][to_string(profile)]["pages"][to_string(page)]["buttons"].erase(to_string(button));
 		ofstream writer(nxsh_config);
-		writer << to_remove.dump(4);
+		writer << CURRENT_ID.config.dump(4);
 		writer.close();
-		read_config(to_remove, CURRENT_ID);
+		read_config(CURRENT_ID);
 	}
 	button_cleared = true;
-	cerr << "cleared\n";
+	LOG("cleared");
 }
 
-void write_config(vector<string> args, size_t arg_size) {	// TODO: modify ID::config instead of using this.
+void write_config(id& ID) {
 	string nxsh_config(getenv("USERPROFILE"));
 	nxsh_config += "\\AppData\\Roaming\\NexusShell";
 	if (!exists(nxsh_config)) create_directory(nxsh_config);
 	nxsh_config += "\\";
-	nxsh_config += args[0];
+	nxsh_config += selected_id;
 	nxsh_config += ".json";
-	json to_write;
-	if (exists(nxsh_config)) {
-		ifstream reader(nxsh_config);
-		if (reader.peek() != ifstream::traits_type::eof()) to_write = json::parse(reader);
-		reader.close();
-	}
-
-	json* current = &to_write;
-	for (size_t i = 0; i < arg_size - 1; ++i) {
-		if (current->find(args[i]) == current->end()) (*current)[args[i]] = json::object();
-		current = &(*current)[args[i]];
-	}
-	*current = args.back();
-
 	ofstream writer(nxsh_config);
-	writer << to_write.dump(4);
+	writer << ID.config.dump(4);
 	writer.close();
 }
--- Server/src/GUI.cpp
+++ Server/src/GUI.cpp
@@ -1,14 +1,13 @@
-#include "../include/GUI.h"
 #include "../include/Header.h"
+#include "../include/GUI.h"
 #include "../include/Config.h"
 
 bool done		   				   = false;
 bool should_draw_button_properties = false;
 bool should_draw_id_properties     = false;
+bool should_draw_settings		   = false;
 bool set_to_null				   = true;
-//bool bad_property				   = false;
 bool clear_dialog_shown			   = false;
-bool press_ok					   = false;
 
 int button_properties_to_draw;
 
@@ -16,7 +15,7 @@ string selected_id = "";
 
 ImGuiWindowFlags im_window_flags = 0;
 
-// NOTE: folder structure, shortcut using folder class containing shortcut vector that uses shortcut/folder class
+// TODO: switch actions to wide strings
 
 int gui_init() {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -24,10 +23,10 @@ int gui_init() {
 	SDL_Window* window = SDL_CreateWindow("NexusShell", 1280, 720, SDL_WINDOW_OPENGL  |
 																 SDL_WINDOW_RESIZABLE |
 																 SDL_WINDOW_HIDDEN   );
-	if (window == nullptr) SDL_ERROR("window")
+	if (window == nullptr) { error_dialog(0, "Failed to initialize window.\n"); exit(-1); }
 	SDL_SetWindowMinimumSize(window, 854, 480);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) SDL_ERROR("renderer")
+	if (renderer == nullptr) { error_dialog(0, "Failed to initialize renderer.\n"); exit(-1); }
 	SDL_ShowWindow(window);
 	// TODO: add silent startup -- no show window
 	IMGUI_CHECKVERSION();
@@ -155,7 +154,6 @@ int tray_init() {		// TODO: Switch to another tray system
 }
 
 void clear_dialog() {
-	clear_dialog_shown = true;
 	ImGui::OpenPopup("Are you sure?");
 	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
 	ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
@@ -168,9 +166,7 @@ void clear_dialog() {
 			clear_dialog_shown = false;
 		}
 		ImGui::SameLine();
-		if (ImGui::Button("No")) { 
-			clear_dialog_shown = false; 
-		}
+		if (ImGui::Button("No")) clear_dialog_shown = false;
 		ImGui::EndPopup();
 	}
 }
@@ -196,22 +192,19 @@ string browse(int type) {
 
 void draw_properties(int index) {
 	ImGui::OpenPopup("Button Properties");
-	const char* types[] = { "File", "URL", "Command" , "Directory"};
+	static const char* types[] = { "File", "URL", "Command", "Directory" };
 	static int original_type;
-	static string original_action;	// TODO: switch to wide strings
-	if (button_cleared) set_to_null = true;
-	if (set_to_null) {
-		original_type = NULL;
-		original_action = "";
-		set_to_null = false;
-		original_type = CURRENT_BUTTON.type;
+	static string original_action;
+	if (set_to_null || button_cleared) {
+		original_type	= CURRENT_BUTTON.type;
 		original_action = CURRENT_BUTTON.action;
-		button_cleared = false;
+		button_cleared	= false;
+		set_to_null		= false;
 	}
 	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
 	ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
 	if (ImGui::BeginPopupModal("Button Properties", NULL, ImGuiWindowFlags_NoResize | 
-														   ImGuiWindowFlags_NoMove)) {
+														  ImGuiWindowFlags_NoMove)) {
 		if (clear_dialog_shown) clear_dialog();
 		ImGui::SetWindowSize(ImVec2(450, 250));
 		ImGui::Text("Button ID: "); ImGui::SameLine(); ImGui::Text(to_string(index).c_str());
@@ -259,8 +252,8 @@ void draw_properties(int index) {
 		}
 		ImGui::BeginDisabled(original_action == "");
 		if (ImGui::Button("OK")) {
-			while (CURRENT_ID.locked) {}
-			CURRENT_ID.locked = true;
+			while (CURRENT_ID.locked.load()) {}
+			CURRENT_ID.locked.exchange(true);
 			if (CURRENT_BUTTON.label_backup != CURRENT_BUTTON.label) {
 				CURRENT_BUTTON.default_label = false;
 				CURRENT_BUTTON.label_backup = CURRENT_BUTTON.label;
@@ -271,21 +264,17 @@ void draw_properties(int index) {
 			original_action = "";
 			set_to_null = true;
 			should_draw_button_properties = false;
-			vector<string> to_write;
-			to_write = { selected_id, "profiles", to_string(CURRENT_ID.current_profile), "pages", to_string(CURRENT_PROFILE.current_page), "buttons", to_string(button_properties_to_draw), "label", CURRENT_BUTTON.label };
-			write_config(to_write, to_write.size());
-			to_write = { selected_id, "profiles", to_string(CURRENT_ID.current_profile), "pages", to_string(CURRENT_PROFILE.current_page), "buttons", to_string(button_properties_to_draw), "has default label", to_string(CURRENT_BUTTON.default_label) };
-			write_config(to_write, to_write.size());
-			to_write = { selected_id, "profiles", to_string(CURRENT_ID.current_profile), "pages", to_string(CURRENT_PROFILE.current_page), "buttons", to_string(button_properties_to_draw), "type", to_string(CURRENT_BUTTON.type) };
-			write_config(to_write, to_write.size());
-			to_write = { selected_id, "profiles", to_string(CURRENT_ID.current_profile), "pages", to_string(CURRENT_PROFILE.current_page), "buttons", to_string(button_properties_to_draw), "action", CURRENT_BUTTON.action };
-			write_config(to_write, to_write.size());
+			CURRENT_ID.config[selected_id]["profiles"][to_string(CURRENT_ID.current_profile)]["pages"][to_string(CURRENT_PROFILE.current_page)]["buttons"][to_string(button_properties_to_draw)]["label"] = CURRENT_BUTTON.label;
+			CURRENT_ID.config[selected_id]["profiles"][to_string(CURRENT_ID.current_profile)]["pages"][to_string(CURRENT_PROFILE.current_page)]["buttons"][to_string(button_properties_to_draw)]["has default label"] = to_string(CURRENT_BUTTON.default_label);
+			CURRENT_ID.config[selected_id]["profiles"][to_string(CURRENT_ID.current_profile)]["pages"][to_string(CURRENT_PROFILE.current_page)]["buttons"][to_string(button_properties_to_draw)]["action"] = CURRENT_BUTTON.action;
+			CURRENT_ID.config[selected_id]["profiles"][to_string(CURRENT_ID.current_profile)]["pages"][to_string(CURRENT_PROFILE.current_page)]["buttons"][to_string(button_properties_to_draw)]["type"] = to_string(CURRENT_BUTTON.type);
+			write_config(CURRENT_ID);
 			reconfigure(CURRENT_ID);
-			CURRENT_ID.locked = false;
+			CURRENT_ID.locked.exchange(false);
 		}
 		ImGui::EndDisabled();
 		ImGui::SameLine();
-		if (ImGui::Button("Clear")) clear_dialog();
+		if (ImGui::Button("Clear")) clear_dialog_shown = true;;
 		ImGui::SameLine();
 		if (ImGui::Button("Cancel")) {
 			CURRENT_BUTTON.label = CURRENT_BUTTON.label_backup;
@@ -309,7 +298,7 @@ void draw_button(string label, int index) {
 		}
 	} else ImGui::Button(std::to_string(index).c_str(), ImVec2(x_size / 6, io.DisplaySize.y / 4));
 }
-// FIXME: Occasional thread crash on disconnect. Extremely rare, but present.
+
 void draw_editor() {
 	ImGuiIO& io = ImGui::GetIO();
 	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0.0f, 0.0f));
@@ -319,17 +308,17 @@ void draw_editor() {
 	ImGui::SetNextWindowPos(ImVec2(176.0f, 0.0f));
 	ImGui::Begin("editor", NULL, im_window_flags);
 	if (selected_id != "") {
+		while (CURRENT_ID.locked.load()) {}
+		CURRENT_ID.locked.exchange(true);
 		for (int i = 1; i <= CURRENT_PROFILE.columns * CURRENT_PROFILE.rows; ++i) {
-			while (ids_locked) {}
-			ids_locked = true;
 			if (!should_draw_id_properties) {
 				if (CURRENT_BUTTON_M1_LOOP.default_label) draw_button(to_string(i), i);
 				else if (CURRENT_BUTTON_M1_LOOP.label.empty()) draw_button("##", i);
 				else draw_button(CURRENT_BUTTON_M1_LOOP.label, i);
 			} else draw_button(to_string(i), i);
 			if (i % CURRENT_PROFILE.columns != 0) ImGui::SameLine();
-			ids_locked = false;
 		}
+		CURRENT_ID.locked.exchange(false);
 	} else {
 		ImGui::BeginDisabled();
 		for (int i = 1; i <= 24; ++i) {
@@ -352,17 +341,19 @@ void draw_editor() {
 void draw_id_properties() {
 	static int columns;
 	static int rows;
+	static string nickname;
 	if (set_to_null) {
 		columns = CURRENT_PROFILE.columns;
 		rows = CURRENT_PROFILE.rows;
+		nickname = CURRENT_ID.nickname;
 		set_to_null = false;
 	}
 	ImGui::OpenPopup("Device Properties");
 	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+	ImGui::SetNextWindowSize(ImVec2(450, 250));
 	ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
 	if (ImGui::BeginPopupModal("Device Properties", NULL, ImGuiWindowFlags_NoMove |
 														  ImGuiWindowFlags_NoResize)) {
-		ImGui::SetWindowSize(ImVec2(450, 250));
 		ImGui::Text("Properties for "); ImGui::SameLine();
 		ImGui::Text(selected_id.c_str());
 		ImGui::SeparatorText("General");
@@ -378,15 +369,12 @@ void draw_id_properties() {
 			CURRENT_PROFILE.rows = rows;
 		}
 		if (ImGui::Button("Ok")) {
-			vector<string> to_write;
-			to_write = { selected_id, "nickname", CURRENT_ID.nickname };
-			write_config(to_write, to_write.size());
-			to_write = { selected_id, "profiles", to_string(CURRENT_ID.current_profile), "columns", to_string(CURRENT_PROFILE.columns) };
-			write_config(to_write, to_write.size());
-			to_write = { selected_id, "profiles", to_string(CURRENT_ID.current_profile), "rows", to_string(CURRENT_PROFILE.rows) };
-			write_config(to_write, to_write.size());
-			while (CURRENT_ID.locked) {}
-			CURRENT_ID.locked = true;
+			while (CURRENT_ID.locked.load()) {}
+			CURRENT_ID.locked.exchange(true);
+			CURRENT_ID.config[selected_id]["nickname"] = CURRENT_ID.nickname;
+			CURRENT_ID.config[selected_id]["profiles"][to_string(CURRENT_ID.current_profile)]["columns"] = to_string(CURRENT_PROFILE.columns);
+			CURRENT_ID.config[selected_id]["profiles"][to_string(CURRENT_ID.current_profile)]["rows"] = to_string(CURRENT_PROFILE.rows);
+			write_config(CURRENT_ID);
 			int new_grid = CURRENT_PROFILE.columns * CURRENT_PROFILE.rows;
 			int old_grid = columns * rows;
 			if (new_grid > old_grid) {
@@ -406,7 +394,7 @@ void draw_id_properties() {
 				CURRENT_PROFILE.buttons = swapper;
 			}
 			reconfigure(CURRENT_ID);
-			CURRENT_ID.locked = false;
+			CURRENT_ID.locked.exchange(false);
 			set_to_null = true;
 			should_draw_id_properties = false;
 		}
@@ -414,6 +402,7 @@ void draw_id_properties() {
 		if (ImGui::Button("Cancel")) {
 			CURRENT_PROFILE.columns = columns;
 			CURRENT_PROFILE.rows = rows;
+			CURRENT_ID.nickname = nickname;
 			set_to_null = true;
 			should_draw_id_properties = false;
 		}
@@ -421,12 +410,68 @@ void draw_id_properties() {
 	}
 }
 
+void draw_settings() {
+	static int temp_port;
+	if (set_to_null) {
+		temp_port = port;
+		set_to_null = false;
+	}
+	ImGui::OpenPopup("Settings");
+	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+	ImGui::SetNextWindowSize(ImVec2(450, 250));
+	ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+	if (ImGui::BeginPopupModal("Settings", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize)) {
+		ImGui::Text("Port: "); ImGui::SameLine();
+		ImGui::InputInt("##Port", &port);
+		if (ImGui::Button("Ok")) {
+			string portstore = getenv("USERPROFILE");			// TODO: move to rw_portstore
+			portstore += "\\AppData\\Roaming\\NexusShell\\";
+			if (!exists(portstore)) create_directory(portstore);
+			portstore += "portstore";
+			ofstream writer(portstore);
+			writer << port;
+			writer.close();
+			set_to_null = true;
+			should_draw_settings = false;
+			restart_socket = true;
+			closesocket(global_sock.ListenSocket);
+			string message = "killreason: port changed to " + to_string(port);
+			for (auto& ID : ids) {
+				send(ID.second.sock.ClientSocket, message.c_str(), message.length() + 1, 0);
+				closesocket(ID.second.sock.ClientSocket);
+			}
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Cancel")) {
+			port = temp_port;
+			set_to_null = true;
+			should_draw_settings = false;
+		}
+		ImGui::EndPopup();
+	}
+}
+
+void draw_sock_reboot() {
+	ImGui::OpenPopup("Rebooting Socket");
+	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+	ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+	if (ImGui::BeginPopupModal("Rebooting Socket", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize)) {
+		ImGui::Text("Rebooting socket.\nPlease wait...");
+		ImGui::EndPopup();
+	}
+}
+
 void draw_main() {
 	ImGuiIO& io = ImGui::GetIO();
-	ImGui::SetNextWindowSize(ImVec2(175, io.DisplaySize.y));
+	if (restart_socket) draw_sock_reboot();
+	if (should_draw_settings) draw_settings(),
+	ImGui::SetNextWindowSize(ImVec2(175, io.DisplaySize.y - 33));
+	ImGui::SetNextWindowSizeConstraints(ImVec2(175, 367), ImVec2(175, FLT_MAX));
 	ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
 	ImGui::Begin("devices", NULL, im_window_flags);
 	ImGui::Text("Connected Devices:\n");
+	while (ids_locked.load()) {}
+	ids_locked.exchange(true);
 	if (!ids.empty()) {
 		if (should_draw_id_properties) draw_id_properties();
 		for (unordered_map<string, id>::iterator it = ids.begin(); it != ids.end(); ++it) {
@@ -452,8 +497,29 @@ void draw_main() {
 		selected_id = "";
 		ImGui::Text("None"); 
 	}
+	ids_locked.exchange(false);
+	ImGui::End();
+	ImGui::SetNextWindowSize(ImVec2(175, 32));
+	ImGui::SetNextWindowSizeConstraints(ImVec2(175, 33), ImVec2(175, 33));
+	ImGui::SetNextWindowPos(ImVec2(0.0f, io.DisplaySize.y - 32));
+	ImGui::Begin("About", NULL, im_window_flags);
+	if (ImGui::Button("Settings")) should_draw_settings = true;
 	ImGui::End();
 	draw_editor();
+}
+
+void error_dialog(int type, string message) {
+	if (!type) message += SDL_GetError();
+	const SDL_MessageBoxButtonData button_data[] = {
+		{SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "OK"}
+	};
+	int buttonid;
+	const SDL_MessageBoxData msgbox_data = {
+		SDL_MESSAGEBOX_ERROR,		NULL,
+		"Error!",					message.c_str(),
+		SDL_arraysize(button_data), button_data,	NULL
+	};
+	SDL_ShowMessageBox(&msgbox_data, &buttonid);
 }
 
 #ifdef _DEBUG 
