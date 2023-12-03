2849ab84e2a3f6551ad78102a61e9b7f8e8f1c0d
Tetrapak0 <112382253+Tetrapak0@users.noreply.github.com>
2023-12-03 10:11:54 +0100

    GitHub Pages Attempt #2

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<div class="header">
+    <nav>
+        <a href="/"><img src="icon.svg" alt="NexusShell" width="25" height="25"></a>
+        <a href="/Downloads">Downloads</a>
+        <a href="https://github.com/Tetrapak0/NexusShell/">GitHub</a>
+    </nav>
+</div>
+
+<slot />
